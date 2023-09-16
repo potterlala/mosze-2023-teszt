12a430c54c10d40ee732a30d91094f70b16caaac
potterlala <potterlala@gmail.com>
2023-09-16 14:10:06 +0200

    main.cpp optimalizálás

--- main.cpp
+++ main.cpp
@@ -5,25 +5,15 @@ constexpr int N_ELEMENTS = 100;
 int main()
 {
     int *b = new int[N_ELEMENTS];
-    std::cout << "1-100 ertekek duplazasa\n";
+    std::cout << "1-100 ertekek duplazasa:\n";
+
+    double atlag = 0;
 
     for (int i = 1; i <= N_ELEMENTS; i++)
     {
         b[i-1] = i * 2;
-    }
-
-    for (int i = 0; i < N_ELEMENTS; i++)
-    {
-        // std::cout << "Ertek: "
-        std::cout << b[i] << std::endl;
-    }    
-
-    std::cout << "\nAtlag szamitasa:\n"; // << std::endl;
-    double atlag = 0;
-
-    for (int i = 0; i < N_ELEMENTS; i++)
-    {
-        atlag += b[i];
+        std::cout << b[i-1] << std::endl;
+        atlag += b[i-1];
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
